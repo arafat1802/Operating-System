2f4db6ca31dca959e83554ba65496dabf33f5750
Md Arafat Hossain <57371182+arafat1802@users.noreply.github.com>
2022-04-13 11:03:55 +0530

    FCFS Upload

---
+++ fcfs.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+int main()
+{
+	int n;
+	cout << "Number of Works: ";
+	cin >> n;
+	int sum = 0;
+	int temp = n;
+	queue<pair<string,int> >q;
+
+	for(int i = 0; i < n; i++){
+		string s;
+		cout << "Process: ";
+		int time = 0;
+		cin >> s;
+		cout << "Burst Time: ";
+		cin >> time;
+		q.push(make_pair(s,sum));
+		sum += (q.front().ss+time);
+	}
+	int waiting_time = 0;
+	cout << "Process\t\t" << "Waitting time\n";
+	while(!q.empty()){
+		cout << q.front().ff << "\t\t" << q.front().ss << "\n";
+		waiting_time += q.front().ss;
+		q.pop();
+	}
+	cout << "\n\navg_waiting_time = " << (double)waiting_time/temp << "\n";
+	return 0;
+}
+/**
+5
+p1 5
+p2 10
+p3 15
+p4 12
+p5 2
+**/
+///first come first serve (CPU scheduling algorithm)
